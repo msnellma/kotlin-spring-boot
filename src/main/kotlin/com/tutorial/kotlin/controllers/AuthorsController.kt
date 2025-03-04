33bbdb3c69bce916dc8776817d6114fc0e4203eb
markussnellman <markus.snellman@gmail.com>
2025-03-04 13:29:24 +0100

    AuthorsController returns ResponseEntity

--- src/main/kotlin/com/tutorial/kotlin/controllers/AuthorsController.kt
+++ src/main/kotlin/com/tutorial/kotlin/controllers/AuthorsController.kt
@@ -4,6 +4,8 @@ import com.tutorial.kotlin.domain.dto.AuthorDto
 import com.tutorial.kotlin.services.AuthorService
 import com.tutorial.kotlin.toAuthorDto
 import com.tutorial.kotlin.toAuthorEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody
 class AuthorsController(private val authorService: AuthorService) {
 
     @PostMapping(path=["/v1/authors"])
-    fun createAuthor(@RequestBody author: AuthorDto): AuthorDto {
-        return authorService.save(author.toAuthorEntity()).toAuthorDto()
+    fun createAuthor(@RequestBody author: AuthorDto): ResponseEntity<AuthorDto> {
+        val createdAuthor = authorService.save(author.toAuthorEntity()).toAuthorDto()
+        return ResponseEntity(createdAuthor, HttpStatus.CREATED)
     }
 }
